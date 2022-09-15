455c653bb7afaa348ee86306ccc5f1688fd35e4e
Corey Murphy <coreyimurphy@gmail.com>
2022-09-14 23:03:48 -0400

    Add: index file connected to restaurant api, re-added users model

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const app = express();
+const axios = require("axios");
+// dot env goes here
+require('dotenv').config();
+
+const encodedParams = new URLSearchParams();
+encodedParams.append("language", "en_US");
+encodedParams.append("limit", "30");
+encodedParams.append("location_id", "297704");
+encodedParams.append("currency", "USD");
+
+const options = {
+  method: 'POST',
+  url: 'https://worldwide-restaurants.p.rapidapi.com/search',
+  headers: {
+    'content-type': 'application/x-www-form-urlencoded',
+    'X-RapidAPI-Key': process.env.apiKEY,
+    'X-RapidAPI-Host': 'worldwide-restaurants.p.rapidapi.com'
+  },
+  data: encodedParams
+};
+
+axios.request(options).then(function (response) {
+	console.log(response.data);
+}).catch(function (error) {
+	console.error(error);
+});
+
+app.listen(8000);
