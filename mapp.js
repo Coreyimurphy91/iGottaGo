61f2744fc97a00e453a28a05d107b6645be00a8c
Corey Murphy <coreyimurphy@gmail.com>
2022-09-22 00:09:46 -0400

    Add: css to sign and log in pages

---
+++ mapp.js
@@ -0,0 +1,20 @@
+// let map;
+
+// function initMap() {
+//   map = new google.maps.Map(document.getElementById("map"), {
+//     center: { lat: -34.397, lng: 150.644 },
+//     zoom: 8,
+//   });
+// }
+
+// window.initMap = initMap;
+
+function initMap() {
+    const options = {
+        center: { lat: -34.397, lng: 150.644 },
+        zoom: 8
+    }
+
+    map = new google.maps.Map(document.getElementById("map"), options)
+
+}
